31878948bac678b89d3af540ebe6744027b2c4f8
Charles Martin <cpm@charlesmartin.com.au>
2019-07-12 10:27:32 +1000

    script to pick up pubs from nime github

---
+++ get_publications.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+wget -q https://github.com/NIME-conference/NIME-bibliography/blob/master/BibTeX/nime.bib -O ./_bibliography/nime.bib
+sed -i'' -e '/@/s/://g' ./_bibliography/nime.bib
+sed -i'' -e '/Date-Added/d' ./_bibliography/nime.bib
+sed -i'' -e '/Date-Modified/d' ./_bibliography/nime.bib
+#sed -i'' -e '/Bdsk-Url-/d' ./_bibliography/nime.bib

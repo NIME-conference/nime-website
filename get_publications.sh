8657aa2fb2bf1c63b2166e64d741d10a403a7b00
Charles Martin <cpm@charlesmartin.com.au>
2019-09-03 14:17:28 +1000

    updated with music archives included

--- get_publications.sh
+++ get_publications.sh
@@ -4,3 +4,5 @@ sed -i'' -e '/@/s/://g' ./_bibliography/nime.bib
 sed -i'' -e '/Date-Added/d' ./_bibliography/nime.bib
 sed -i'' -e '/Date-Modified/d' ./_bibliography/nime.bib
 #sed -i'' -e '/Bdsk-Url-/d' ./_bibliography/nime.bib
+rm ./_bibliography/nime.bib-e
+wget -q https://raw.githubusercontent.com/NIME-conference/NIME-bibliography/master/music_proceedings/nime_music.bib -O ./_bibliography/nime_music.bib

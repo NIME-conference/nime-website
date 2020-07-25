bc2284919ee945d44d5cd35b9d56e0800f051ed4
Charles Martin <cpm@charlesmartin.com.au>
2020-07-25 18:57:01 +1000

    little script to update bibtex files when necessary

--- get_publications.sh
+++ get_publications.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-wget -q https://raw.githubusercontent.com/NIME-conference/NIME-bibliography/master/BibTeX/nime.bib -O ./_bibliography/nime.bib
-sed -i'' -e '/@/s/://g' ./_bibliography/nime.bib
-sed -i'' -e '/Date-Added/d' ./_bibliography/nime.bib
-sed -i'' -e '/Date-Modified/d' ./_bibliography/nime.bib
-#sed -i'' -e '/Bdsk-Url-/d' ./_bibliography/nime.bib
-rm ./_bibliography/nime.bib-e
-wget -q https://raw.githubusercontent.com/NIME-conference/NIME-bibliography/master/music_proceedings/nime_music.bib -O ./_bibliography/nime_music.bib
+wget -q https://github.com/NIME-conference/NIME-bibliography/raw/master/paper_proceedings/nime_papers.bib -O ./_bibliography/nime_papers.bib
+wget -q https://github.com/NIME-conference/NIME-bibliography/raw/master/music_proceedings/nime_music.bib -O ./_bibliography/nime_music.bib
+wget -q https://github.com/NIME-conference/NIME-bibliography/raw/master/installation_proceedings/nime_installations.bib -O ./_bibliography/nime_installations.bib
+# some example find-and-replace routines (uses BSD sed for macOS)
+#sed -i'' -e '/@/s/://g' ./_bibliography/publications.bib
+#sed -i'' -e '/Date-Added/d' ./_bibliography/publications.bib
+#sed -i'' -e '/Date-Modified/d' ./_bibliography/publications.bib
+#sed -i'' -e '/Bdsk-Url-/d' ./_bibliography/publications.bib
+
+
+

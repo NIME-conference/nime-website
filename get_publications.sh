fa4ca18cdecced10a05e34fa981f21e3a64ced4c
Charles Martin <cpm@charlesmartin.com.au>
2022-12-06 13:59:18 +1100

    fixed getting of installations bibtex

--- get_publications.sh
+++ get_publications.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 wget -q http://nime-conference.github.io/NIME-bibliography/nime_papers.bib -O ./_bibliography/nime_papers.bib
 wget -q http://nime-conference.github.io/NIME-bibliography/nime_music.bib -O ./_bibliography/nime_music.bib
-wget -q http://nime-conference.github.io/NIME-bibliography/nime_installation.bib -O ./_bibliography/nime_installations.bib
+wget -q http://nime-conference.github.io/NIME-bibliography/nime_installations.bib -O ./_bibliography/nime_installations.bib

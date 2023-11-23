901cb8034523ef7d3b178fa3cd05635eda000e44
cpmpercussion <cpm@charlesmartin.com.au>
2023-11-23 03:21:02 +0000

    Deploying to gh-pages from @ NIME-conference/nime-website@d8dade4998b95882a68070601f589cb38301072b 🚀

---
+++ get_publications.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+wget -q http://nime-conference.github.io/NIME-bibliography/nime_papers.yaml -O ./_data/nime_papers.yaml
+wget -q http://nime-conference.github.io/NIME-bibliography/nime_music.yaml -O ./_data/nime_music.yaml
+wget -q http://nime-conference.github.io/NIME-bibliography/nime_installations.yaml -O ./_data/nime_installations.yaml

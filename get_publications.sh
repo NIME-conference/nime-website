d9ed425676e0465209c9a004908407d74fe9c335
stefanofasciani <stefanofasciani@stefanofasciani.com>
2024-09-09 08:47:27 +0200

    Update get_publications.sh

--- get_publications.sh
+++ get_publications.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget -q http://nime-conference.github.io/NIME-bibliography/nime_papers.yaml -O ./_data/nime_papers.yaml
-wget -q http://nime-conference.github.io/NIME-bibliography/nime_music.yaml -O ./_data/nime_music.yaml
-wget -q http://nime-conference.github.io/NIME-bibliography/nime_installations.yaml -O ./_data/nime_installations.yaml
+wget -q https://nime-conference.github.io/NIME-bibliography/nime_papers.yaml -O ./_data/nime_papers.yaml
+wget -q https://nime-conference.github.io/NIME-bibliography/nime_music.yaml -O ./_data/nime_music.yaml
+wget -q https://nime-conference.github.io/NIME-bibliography/nime_installations.yaml -O ./_data/nime_installations.yaml
